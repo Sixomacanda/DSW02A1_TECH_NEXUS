f451ee657300baff952b5289e23d5027bdcfc330
AmahleNgcobo12 <amahleenhlerh5@gmail.com>
2026-05-22 17:11:00 +0200

    Changes

--- google-auth-server/google_server.js
+++ google-auth-server/google_server.js
@@ -37,7 +37,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback",
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
         },
         function (accessToken, refreshToken, profile, done) {
             return done(null, profile);
