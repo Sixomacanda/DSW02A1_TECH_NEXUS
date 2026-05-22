8b769c966fa89c7b3c1e6c566ec19f1789847d16
AmahleNgcobo12 <amahleenhlerh5@gmail.com>
2026-05-22 17:21:20 +0200

    Changes

--- google-auth-server/google_server.js
+++ google-auth-server/google_server.js
@@ -19,7 +19,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI,
+        mongoUrl: process.env.MONGO_URL,
         collectionName: "sessions"
     }),
     cookie: {
