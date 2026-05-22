b1677908d82f3f73f289507d82cb6f9a85f131a9
AmahleNgcobo12 <amahleenhlerh5@gmail.com>
2026-05-22 17:27:17 +0200

    Changes

--- google-auth-server/google_server.js
+++ google-auth-server/google_server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 const MongoStore = require("connect-mongo").MongoStore;
 
 // Session setup
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
